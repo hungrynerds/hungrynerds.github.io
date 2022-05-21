4a44cd0c9422da177cc69b6229cba76243aa13ca
René Kuhn <55456939+RocketRene@users.noreply.github.com>
2022-05-21 18:41:43 +0200

    update edit URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,14 +38,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/hungrynerds/hungrynerds.github.io/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/hungrynerds/hungrynerds.github.io/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
