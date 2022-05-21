564dcaa178c8072ce6e2d20a3b0d9391510d4f18
René Kuhn <55456939+RocketRene@users.noreply.github.com>
2022-05-21 18:30:22 +0200

    edit GH organisation & Project name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,8 +17,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'SunlightLyn', // Usually your GitHub org/user name.
-  projectName: 'docusaurus-templates', // Usually your repo name.
+  organizationName: 'hungrynerds', // Usually your GitHub org/user name.
+  projectName: 'hungrynerds.github.io', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
